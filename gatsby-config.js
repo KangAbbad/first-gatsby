9cb62b8ec3a0b50e211b7c8623537c84c15216db
kangabbad <naufalabbad9@gmail.com>
2020-08-11 21:30:39 +0700

    initial test

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,26 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Learn GatsbyJS',
+        short_name: 'Learn GatsbyJS',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#007900',
+        display: 'standalone',
+        icon: 'src/images/favicon.png',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+         workboxConfig: {
+            globPatterns: ['**/icon-path*']
+         }
+      }
+   },
+  ],
 }
